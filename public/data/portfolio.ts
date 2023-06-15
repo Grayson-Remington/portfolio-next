45b222b1c19a7ca758d66c0ed3b26f33f3308993
Grayson-Remington <graysonbremington@gmail.com>
2023-06-15 12:50:34 -0400

    stack updates to the portfolio

--- public/data/portfolio.ts
+++ public/data/portfolio.ts
@@ -2,25 +2,19 @@ export default [
   {
     title: "Distance Between",
     imgUrl: "assets/distance-between.jpg",
-    stack: [
-      "NextJS",
-      "TypeScript",
-
-      "ChatGPT & Google Maps Integration",
-      "Database Integration",
-    ],
+    stack: ["NextJS", "TypeScript", "Prisma", "TRPC", "ChatGPT", "Google Maps"],
     link: "https://distance-between-next-database.vercel.app/",
   },
   {
     title: "Music Notes Generator",
     imgUrl: "assets/music-note-generator.jpg",
-    stack: ["React", "Javascript", "CSS"],
+    stack: ["React", "Javascript", "CSS", "JQuery", "MIDI"],
     link: "https://simple-music-generator.vercel.app/",
   },
   {
     title: "Dead Drift Flycasters",
     imgUrl: "assets/dead-drift-flycasters.jpg",
-    stack: ["React", "Javascript", "Tailwind"],
+    stack: ["React", "Javascript", "Tailwind", "JQuery"],
     link: "https://grayson-remington.github.io/dead-drift-flycasters/",
   },
 ];
